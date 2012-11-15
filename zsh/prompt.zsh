a332db669ed35c61b779d3062c80b13556542ec7
Alex Smith <alex.geoffrey.smith@gmail.com>
2012-11-15 22:11:20 +1100

    git status in prompt doesn't cause tab indent on ctrl+r

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,13 +1,12 @@
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}%{$fg[white]%}["
-ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="\e[0;31m●\e[0m"
+autoload -U colors && colors
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}●%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 # Customized git status
 git_custom_status() {
   local cb=$(current_branch)
   if [ -n "$cb" ]; then
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(current_branch)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "[$(current_branch)$(parse_git_dirty)]"
   fi
 }
 

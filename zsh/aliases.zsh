c1821123f4c7b22d431bb22d3d707e585edcddc1
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-02-12 22:50:42 +1100

    I AM WASTING MY LIFE TYPING AN EXTRA G

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,9 +14,9 @@ alias pdata='be rake db:reset db:production:data_light db:production:load_data d
 # Git aliases
 alias g='git'
 
-alias ggpush='git push origin $(current_branch)'
+alias gpush='git push origin $(current_branch)'
 
-alias ggup='git fetch && git rebase origin/$(current_branch)'
+alias gup='git fetch && git rebase origin/$(current_branch)'
 
 alias gap='git add -p'
 

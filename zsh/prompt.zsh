81d4dde991811fa96a5408f310109a23dfe3db7c
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-08-08 23:37:06 +1000

    Strip out unused git stuff.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,13 +1,16 @@
 autoload -U colors && colors
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}●%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 # Customized git status
-git_custom_status() {
+git_status() {
   local cb=$(current_branch)
   if [ -n "$cb" ]; then
-    echo "[$(current_branch)$(parse_git_dirty)]"
+    echo "[$(current_branch)]"
   fi
 }
 
-PROMPT='%2~ $(git_custom_status)$%b '
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+PROMPT='%2~ $(git_status)$%b '

cc8eb45273845e20db4b22f6ae50af3f82ab7ddb
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-08-08 12:30:31 +1000

    Fast specs alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -43,3 +43,5 @@ alias rr='rake routes'
 ermahgerd() {
   curl -d "$*" http://erpers.herokuapp.com/ | say
 }
+
+alias fs='grep -RL --exclude turnip_helper.rb --exclude spec_helper.rb --exclude-dir spec/acceptance --exclude-dir spec/support --exclude-dir spec/steps "spec_helper" spec | xargs rspec'
